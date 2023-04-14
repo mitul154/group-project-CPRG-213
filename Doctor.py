1b12acdf65ab392f7a00e0f41b82acd9a7aa57aa
Bop <122827184+TooBoppity@users.noreply.github.com>
2023-04-14 15:44:14 -0600

    Add constructor and getters for doctor class

--- Doctor.py
+++ Doctor.py
@@ -1,3 +1,35 @@
 class Doctor:
-    def __init__(doctor_id, name, specialization, working_time, qualification, room_number): 
+    def __init__(self, doctor_id, name, specialization, working_time, qualification, room_number): 
+        self.doctor_id = doctor_id
+        self.name = name
+        self.specialization = specialization    
+        self.working_time = working_time
+        self.qualification = qualification
+        self.room_number = room_number
         
+        
+    def get_doctor_id(self):
+        return self.doctor_id
+    
+    
+    def get_name(self):
+        return self.name
+    
+    
+    def get_specialization(self):
+        return self.specialization
+    
+    
+    def get_working_time(self):
+        return self.working_time
+    
+    
+    def get_qualification(self ):
+        return self.qualification
+    
+    
+    def get_room_number(self):
+        return self.room_number
+    
+    
+    
