7a82bedcc42d2ad041d1ae04bd4c8b6ea08552ec
Bop <122827184+TooBoppity@users.noreply.github.com>
2023-04-14 16:04:49 -0600

    Add finished Doctor Class with 1 object created

--- Doctor.py
+++ Doctor.py
@@ -1,35 +1,52 @@
 class Doctor:
-    def __init__(self, doctor_id, name, specialization, working_time, qualification, room_number): 
+    def __init__(self, doctor_id, doctor_name, specialization, working_time, qualification, room_number):
         self.doctor_id = doctor_id
-        self.name = name
-        self.specialization = specialization    
+        self.doctor_name = doctor_name
+        self.specialization = specialization
         self.working_time = working_time
         self.qualification = qualification
         self.room_number = room_number
-        
-        
+
     def get_doctor_id(self):
         return self.doctor_id
-    
-    
-    def get_name(self):
-        return self.name
-    
-    
+
+    def get_doctor_name(self):
+        return self.doctor_name
+
     def get_specialization(self):
         return self.specialization
-    
-    
+
     def get_working_time(self):
         return self.working_time
-    
-    
-    def get_qualification(self ):
+
+    def get_qualification(self):
         return self.qualification
-    
-    
+
     def get_room_number(self):
         return self.room_number
-    
-    
-    
+
+    def set_doctor_id(self, new_id):
+        self.doctor_id = new_id
+
+    def set_new_name(self, new_name):
+        self.name = new_name
+
+    def set_new_specialization(self, new_specialization):
+        self.specialization = new_specialization
+
+    def set_new_working_time(self, new_working_time):
+        self.working_time = new_working_time
+
+    def set_new_qualification(self, new_qualification):
+        self.qualification = new_qualification
+
+    def set_new_room_number(self, new_room_number):
+        self.room_number = new_room_number
+
+    def __str__(self):
+        return f"{self.doctor_id} | {self.doctor_name} | {self.specialization} | {self.working_time} | {self.qualification} | {self.room_number}"
+
+
+Doctor1 = Doctor(12, "Bop", "CANCER", "11 am - 12 pm", "MD", 202)
+
+print(Doctor1)
