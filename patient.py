76570f6d1e702aab75c36c52c89126efbeaf778f
Bop <122827184+TooBoppity@users.noreply.github.com>
2023-04-14 17:19:13 -0600

    Added patient.py

--- patient.py
+++ patient.py
@@ -21,23 +21,25 @@ class Patient:
     def get_age(self):
         return self.age
 
-    def set_pid(self, pid):
-        self.pid = pid
+    def set_pid(self, new_id):
+        self.pid = new_id
+        return self.pid
 
-    def set_name(self, name):
-        self.name = name
+    def set_name(self, new_name):
+        self.name = new_name
+        return self.name
 
-    def set_disease(self, disease):
-        self.disease = disease
+    def set_disease(self, new_disease):
+        self.disease = new_disease
+        return self.disease
 
-    def set_gender(self, gender):
-        self.gender = gender
+    def set_gender(self, new_gender):
+        self.gender = new_gender
+        return self.gender
 
-    def set_age(self, age):
-        self.age = age
+    def set_id(self, new_age):
+        self.age = new_age
+        return self.age
 
     def __str__(self):
         return f"{self.pid}_{self.name}_{self.disease}_{self.gender}_{self.age}"
-
-
-
